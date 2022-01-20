08ccbccaf4e971e1fbbad1a6c9d433ba076f81e7
Steve Ryan <steve.ryan@asu.edu>
2022-01-20 14:17:59 -0700

    Install composer, PHPCS checks.

--- pitchfork-blocks.php
+++ pitchfork-blocks.php
@@ -3,7 +3,7 @@
  * Plugin Name:     Pitchfork Blocks
  * Plugin URI:      http://github.com/ASU-Engineering/Pitchfork-Blocks
  * Description:     Contains blocks that correspond to the universal design system within ASU. Leverages Bootstrap 4 and Advanced Custom Fields (ACF).
- * Author:          Steve Ryab
+ * Author:          Steve Ryan
  * Author URI:      https://engineering.asu.edu
  * Text Domain:     pitchfork-blocks
  * Domain Path:     /languages
