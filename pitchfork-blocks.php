5e2ec0b1c8dcf694e18b758521b8d6bc73f0f72d
Steve Ryan <steve.ryan@asu.edu>
2022-01-20 12:01:53 -0700

    Initial commit.

---
+++ pitchfork-blocks.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Pitchfork Blocks
+ * Plugin URI:      http://github.com/ASU-Engineering/Pitchfork-Blocks
+ * Description:     Contains blocks that correspond to the universal design system within ASU. Leverages Bootstrap 4 and Advanced Custom Fields (ACF).
+ * Author:          Steve Ryab
+ * Author URI:      https://engineering.asu.edu
+ * Text Domain:     pitchfork-blocks
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Pitchfork_Blocks
+ */
+
+// Your code starts here.
