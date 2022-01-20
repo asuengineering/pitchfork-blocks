575ca55378b74e145681648ab2d6ffbb624e6939
Steve Ryan <steve.ryan@asu.edu>
2022-01-20 15:50:48 -0700

    TGMPA checks for ACF Pro. Basic ACF JSON config.

---
+++ inc/acf-config.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * ACF Configurations. 
+ * - Add save/load points for JSON feature.
+ */
+
+// Register a new save point for the Local JSON feature for this plugin.
+add_filter( 'acf/settings/save_json', 'pitchfork_blocks_acf_json_save_point' );
+function pitchfork_blocks_acf_json_save_point( $path ) {
+	$path = dirname( __FILE__ ) . '/acf-json';
+	return $path;
+}
+
+// It needs to load as well.
+add_filter( 'acf/settings/load_json', 'pitchfork_blocks_acf_json_load_point' );
+function pitchfork_blocks_acf_json_load_point( $paths ) {
+	$paths[] = dirname( __FILE__ ) . '/acf-json';
+	return $paths;
+}
+
+// // Register new options page within ACF
+// if( function_exists('acf_add_options_page') ) {
+// 	acf_add_options_sub_page(
+// 		array(
+// 			'page_title' 	=> 'Pitchfork Blocks Options',
+// 			'menu_title'	=> 'Pitchfork Blocks',
+// 			'parent_slug'	=> 'tools.php',
+// 		)
+// 	);
+// }
--- pitchfork-blocks.php
+++ pitchfork-blocks.php
@@ -8,8 +8,24 @@
  * Text Domain:     pitchfork-blocks
  * Domain Path:     /languages
  * Version:         0.1.0
- *
+ * 
  * @package         Pitchfork_Blocks
+ * 
+ * GitHub Plugin URI: http://github.com/ASU-Engineering/Pitchfork-Blocks
  */
 
-// Your code starts here.
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// Function: Activate
+// Function: Deactivate
+// Function: Execute plugin
+
+// TGM Plugin Activation Script. Checks for Advanced Custom Fields.
+require_once dirname( __FILE__ ) . '/tgmpa/class-tgm-plugin-activation.php';
+require_once dirname( __FILE__ ) . '/tgmpa/dependency-check.php';
+
+// ACF configurations.
+require_once dirname( __FILE__ ) . '/inc/acf-config.php';
