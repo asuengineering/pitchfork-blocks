c03423856f08939f0cee46f800cf1d8a82269d38
Steve Ryan <steve.ryan@asu.edu>
2022-04-07 16:13:26 -0700

    Make sure theme.css makes it into editor screen.

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -14,18 +14,20 @@ function pitchfork_blocks_enqueue_block_styles() {
 		$the_version    = $the_plugin['Version'];
 		$plugin_version = $the_version . '.' . filemtime( plugin_dir_path( __DIR__ ) . 'css/theme.min.css' );
 	
-		wp_enqueue_style( 'pitchfork-block-styles-frank', plugin_dir_url( __DIR__ ) . 'css/theme.min.css', array(), $plugin_version );
+		wp_enqueue_style( 'pitchfork-block-styles', plugin_dir_url( __DIR__ ) . 'css/theme.min.css', array(), $plugin_version );
 }
 
 // Main styles/scripts enqueue.
 add_action( 'enqueue_block_editor_assets', 'pitchfork_blocks_enqueue_block_scripts' );
 function pitchfork_blocks_enqueue_block_scripts() {
 
-		$the_plugin     = get_plugin_data( plugin_dir_path( __DIR__ ) . 'pitchfork-blocks.php' );
-		$the_version    = $the_plugin['Version'];
-		$plugin_version = $the_version . '.' . filemtime( plugin_dir_path( __DIR__ ) . 'js/block-variations.js' );
+	$the_plugin     = get_plugin_data( plugin_dir_path( __DIR__ ) . 'pitchfork-blocks.php' );
+	$the_version    = $the_plugin['Version'];
+	$block_variations_version = $the_version . '.' . filemtime( plugin_dir_path( __DIR__ ) . 'js/block-variations.js' );
+	$block_styles_version = $the_version . '.' . filemtime( plugin_dir_path( __DIR__ ) . 'css/theme.min.css' );
 	
-	wp_enqueue_script( 'uds-block-variations', plugin_dir_url( __DIR__ ) . 'js/block-variations.js',  array( 'wp-blocks', 'wp-dom' ), $plugin_version , true );
+	wp_enqueue_script( 'uds-block-variations', plugin_dir_url( __DIR__ ) . 'js/block-variations.js',  array( 'wp-blocks', 'wp-dom' ), $block_variations_version , true );
+	wp_enqueue_style( 'pitchfork-block-styles', plugin_dir_url( __DIR__ ) . 'css/theme.min.css', array(), $block_styles_version );
 
 }
 
