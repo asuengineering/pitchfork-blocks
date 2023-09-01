ba2210e243e7800d1b73ac70f6500bf1a77afe62
Steve Ryan <steve.ryan@asu.edu>
2023-09-01 15:13:29 -0700

    Enqueue block editor style script correctly.

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -14,30 +14,23 @@ function pitchfork_blocks_enqueue_block_styles() {
 		$the_version        = $the_plugin['Version'];
 		$plugin_version     = $the_version . '.' . filemtime( plugin_dir_path( __DIR__ ) . 'dist/css/blocks.css' );
 		$hero_video_version = $the_version . '.' . filemtime( plugin_dir_path( __DIR__ ) . 'dist/js/hero-video.js' );
+		$block_variations_version = $the_version . '.' . filemtime( plugin_dir_path( __DIR__ ) . 'dist/js/block-variations.js' );
 
 		wp_enqueue_style( 'pitchfork-block-styles', plugin_dir_url( __DIR__ ) . 'dist/css/blocks.css', array( 'pitchfork-styles' ), $plugin_version );
 		wp_enqueue_script( 'hero-video-controls', plugin_dir_url( __DIR__ ) . 'dist/js/hero-video.js', array(), $hero_video_version, true );
-}
-
-// Main styles/scripts enqueue.
-add_action( 'enqueue_block_editor_assets', 'pitchfork_blocks_enqueue_block_scripts' );
-function pitchfork_blocks_enqueue_block_scripts() {
-
-	$the_plugin               = get_plugin_data( plugin_dir_path( __DIR__ ) . 'pitchfork-blocks.php' );
-	$the_version              = $the_plugin['Version'];
-	$block_variations_version = $the_version . '.' . filemtime( plugin_dir_path( __DIR__ ) . 'dist/js/block-variations.js' );
-	$block_styles_version     = $the_version . '.' . filemtime( plugin_dir_path( __DIR__ ) . 'dist/css/blocks.css' );
-	$hero_video_version       = $the_version . '.' . filemtime( plugin_dir_path( __DIR__ ) . 'dist/js/hero-video.js' );
-
-	wp_enqueue_script( 'uds-block-variations', plugin_dir_url( __DIR__ ) . 'dist/js/block-variations.js', array( 'wp-blocks', 'wp-dom' ), $block_variations_version, true );
-	wp_enqueue_script( 'hero-video-controls', plugin_dir_url( __DIR__ ) . 'dist/js/hero-video.js', array(), $hero_video_version, true );
-	wp_enqueue_style( 'pitchfork-block-styles', plugin_dir_url( __DIR__ ) . 'dist/css/blocks.min.css', array(), $block_styles_version );
+		wp_enqueue_script( 'uds-block-variations', plugin_dir_url( __DIR__ ) . 'dist/js/block-variations.js', array( 'wp-blocks', 'wp-dom' ), $block_variations_version, true );
 
 }
+
+// Allow styles added here to also be present within the block editor.
+function pitchfork_blocks_gutenberg_css() {
+	add_editor_style( plugin_dir_url( __DIR__ ) . 'dist/css/blocks.css' );
+}
+add_action( 'after_setup_theme', 'pitchfork_blocks_gutenberg_css' );
 
 // Enqueue to the admin. Gutenberg editor fixes.
-add_action( 'admin_enqueue_scripts', 'pitchfork_blocks_enqueue_admin_scripts' );
-function pitchfork_blocks_enqueue_admin_scripts() {
+add_action( 'enqueue_block_editor_assets', 'pitchfork_blocks_enqueue_block_editor_scripts' );
+function pitchfork_blocks_enqueue_block_editor_scripts() {
 
 	$the_plugin     = get_plugin_data( plugin_dir_path( __DIR__ ) . 'pitchfork-blocks.php' );
 	$the_version    = $the_plugin['Version'];
