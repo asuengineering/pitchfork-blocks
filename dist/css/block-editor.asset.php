920a760ce20a8f9e06a9b28eb363623f2cfe9ca5
Steve Ryan <steve.ryan@asu.edu>
2023-09-01 10:13:52 -0700

    Adjust block editor specific SASS

--- dist/css/block-editor.asset.php
+++ dist/css/block-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('version' => 'c2047946c834ea6dbba6a31d1562ad03', 'dependencies' => array());
+<?php return array('version' => '420129ce1f018aa6ca652bf204b9240e', 'dependencies' => array());
