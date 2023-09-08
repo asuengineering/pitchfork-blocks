041ed85d4843d3c1928d326c1610236f35a518a6
Steve Ryan <steve.ryan@asu.edu>
2023-09-08 12:21:12 -0700

    Register ACF save point for PF Blocks plugin

--- inc/acf-config.php
+++ inc/acf-config.php
@@ -1,22 +1,13 @@
 <?php
 /**
- * ACF Configurations.
- * - Add save/load points for JSON feature.
+ * Additional functions for Advanced Custom Fields.
+ *
+ * Contents:
+ *   - Load path for ACF groups from the parent.
+ *   - Register custom blocks for the theme.
  *
  * @package pitchfork-blocks
  */
-
-// /**
-//  * Register a new save point for the Local JSON feature for this plugin.
-//  *
-//  * @param  mixed $path // path to ACF safe point.
-//  * @return $path
-//  */
-// function pitchfork_blocks_acf_json_save_point( $path ) {
-// 	$path = PITCHFORK_BLOCKS_BASE_PATH . '/acf-json';
-// 	return $path;
-// }
-// add_filter( 'acf/settings/save_json', 'pitchfork_blocks_acf_json_save_point' );
 
 /**
  * Register a new loading point for the Local JSON feature for this plugin.
@@ -30,13 +21,45 @@ function pitchfork_blocks_acf_json_load_point( $paths ) {
 }
 add_filter( 'acf/settings/load_json', 'pitchfork_blocks_acf_json_load_point' );
 
-// // Register new options page within ACF
-// if( function_exists('acf_add_options_page') ) {
-// acf_add_options_sub_page(
-// array(
-// 'page_title'    => 'Pitchfork Blocks Options',
-// 'menu_title'    => 'Pitchfork Blocks',
-// 'parent_slug'   => 'tools.php',
-// )
-// );
-// }
+
+/**
+ * Create a save point for specifc JSON files for the the plugin's ACF groups.
+ *
+ * Key list
+ * - UDS Block: Accordion - group_627bf21169d29
+ * - UDS Block: Alert - group_603436127871b
+ * - UDS Block: Banner - group_611ec0462e2e5
+ * - UDS Block: Background Section - group_603819e0955bf
+ * - UDS Block: Blockquote - group_6272d7b38757e
+ * - UDS Block: Breadcrumb - group_6267257b9dd8f
+ * - UDS Block: Card - group_602edd7eb0e61
+ * - UDS Block: Content-Image Overlap - group_602314dddcc3f
+ * - UDS Block: Foldable Card - group_613fc14daf233
+ * - UDS Block: Grid Links - group_60bf99d0b5ceb
+ * - UDS Block: Hero - group_6114252e73759
+ * - UDS Block: Hero (Video) - group_62ead7abbe15f
+ * - UDS Block: Sidebar - group_62689708eb27e
+ * - UDS Block: Subtitle - group_6250abdba7d2c
+ * - UDS Fieldset: Button - group_601db250e6c94
+ *
+ * @return $paths
+ */
+function pitchfork_blocks_field_groups( $path ) {
+    $path = get_stylesheet_directory() . '/acf-json';
+    return $path;
+}
+add_filter( 'acf/settings/save_json/key=group_627bf21169d29', 'pitchfork_blocks_field_groups' );
+add_filter( 'acf/settings/save_json/key=group_603436127871b', 'pitchfork_blocks_field_groups' );
+add_filter( 'acf/settings/save_json/key=group_611ec0462e2e5', 'pitchfork_blocks_field_groups' );
+add_filter( 'acf/settings/save_json/key=group_603819e0955bf', 'pitchfork_blocks_field_groups' );
+add_filter( 'acf/settings/save_json/key=group_6272d7b38757e', 'pitchfork_blocks_field_groups' );
+add_filter( 'acf/settings/save_json/key=group_6267257b9dd8f', 'pitchfork_blocks_field_groups' );
+add_filter( 'acf/settings/save_json/key=group_602edd7eb0e61', 'pitchfork_blocks_field_groups' );
+add_filter( 'acf/settings/save_json/key=group_602314dddcc3f', 'pitchfork_blocks_field_groups' );
+add_filter( 'acf/settings/save_json/key=group_613fc14daf233', 'pitchfork_blocks_field_groups' );
+add_filter( 'acf/settings/save_json/key=group_60bf99d0b5ceb', 'pitchfork_blocks_field_groups' );
+add_filter( 'acf/settings/save_json/key=group_6114252e73759', 'pitchfork_blocks_field_groups' );
+add_filter( 'acf/settings/save_json/key=group_62ead7abbe15f', 'pitchfork_blocks_field_groups' );
+add_filter( 'acf/settings/save_json/key=group_62689708eb27e', 'pitchfork_blocks_field_groups' );
+add_filter( 'acf/settings/save_json/key=group_6250abdba7d2c', 'pitchfork_blocks_field_groups' );
+add_filter( 'acf/settings/save_json/key=group_601db250e6c94', 'pitchfork_blocks_field_groups' );
