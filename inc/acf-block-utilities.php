33b4bcf60c1863472244ed130fae45e7ce23494f
Steve Ryan <steve.ryan@asu.edu>
2023-10-11 22:20:18 -0700

    Remove ACF inner block wrapper filter function.
    Function could malfunction when the same ACF action was recalled/refiltered within another plugin. Solution is to add CSS rules to deal with the wrapper rather than surpress it.

--- inc/acf-block-utilities.php
+++ inc/acf-block-utilities.php
@@ -29,27 +29,3 @@ function pitchfork_blocks_acf_calculate_spacing( $block ) {
 	return $style;
 	// echo '<div style="' . $style . '">Block content</div>';
 }
-
-/**
- * Create filter to remove ACF inner blocks wrapper from a couple of blocks.
- *
- * Note that some additional CSS to "ignore" the wrapper in a grid context is required
- * for any of these blocks to function correctly in the block editor. The wrapper is
- * non-removable in that context.
- *
- * Removed here for greater transparency and compliance with actual markup for Unity elements.
- *
- * See: https://www.advancedcustomfields.com/resources/whats-new-with-acf-blocks-in-acf-6/#block-versioning
- */
-add_filter( 'acf/blocks/wrap_frontend_innerblocks', 'pitchfork_acf_remove_wrap_innerblocks', 10, 2 );
-function pitchfork_acf_remove_wrap_innerblocks( $wrap, $name ) {
-
-	$nowrap_block_names = array( 'acf/hero', 'acf/hero-video' );
-
-	// Loop through the array above. If located, remove the wrapper.
-	if ( in_array( $name, $nowrap_block_names ) ) {
-		return false;
-	}
-
-	return true;
-}
