ece9ef53e7a0fc7201c9fea1008fd0fa85862d1b
Steve Ryan <steve.ryan@asu.edu>
2024-04-16 13:39:36 -0700

    Add ACF save points for new blocks

--- inc/acf-config.php
+++ inc/acf-config.php
@@ -33,7 +33,6 @@ add_filter( 'acf/settings/load_json', 'pitchfork_blocks_acf_json_load_point' );
  * - UDS Block: Blockquote - group_6272d7b38757e
  * - UDS Block: Breadcrumb - group_6267257b9dd8f
  * - UDS Block: Card - group_602edd7eb0e61
- * - UDS Block: Card v2 - group_6605e470e665f
  * - UDS Block: Content-Image Overlap - group_602314dddcc3f
  * - UDS Block: Foldable Card - group_613fc14daf233
  * - UDS Block: Grid Links - group_60bf99d0b5ceb
@@ -42,6 +41,12 @@ add_filter( 'acf/settings/load_json', 'pitchfork_blocks_acf_json_load_point' );
  * - UDS Block: Sidebar - group_62689708eb27e
  * - UDS Block: Subtitle - group_6250abdba7d2c
  * - UDS Fieldset: Button - group_601db250e6c94
+ * - UDS Block: Card v2 - group_6605e470e665f
+ * - UDS Block: Card v2, Event Date - group_661d8dcf74644
+ * - UDS Block: Card v2, Image - group_6610755895003
+ * - UDS Block: Card v2, Link - group_66196d432370c
+ * - UDS Block: Card v2, Tag - group_6619bf2ed4349
+ *
  *
  * @return $paths
  */
@@ -56,7 +61,6 @@ add_filter( 'acf/settings/save_json/key=group_603819e0955bf', 'pitchfork_blocks_
 add_filter( 'acf/settings/save_json/key=group_6272d7b38757e', 'pitchfork_blocks_field_groups' );
 add_filter( 'acf/settings/save_json/key=group_6267257b9dd8f', 'pitchfork_blocks_field_groups' );
 add_filter( 'acf/settings/save_json/key=group_602edd7eb0e61', 'pitchfork_blocks_field_groups' );
-add_filter( 'acf/settings/save_json/key=group_6605e470e665f', 'pitchfork_blocks_field_groups' );
 add_filter( 'acf/settings/save_json/key=group_602314dddcc3f', 'pitchfork_blocks_field_groups' );
 add_filter( 'acf/settings/save_json/key=group_613fc14daf233', 'pitchfork_blocks_field_groups' );
 add_filter( 'acf/settings/save_json/key=group_60bf99d0b5ceb', 'pitchfork_blocks_field_groups' );
@@ -65,4 +69,8 @@ add_filter( 'acf/settings/save_json/key=group_62ead7abbe15f', 'pitchfork_blocks_
 add_filter( 'acf/settings/save_json/key=group_62689708eb27e', 'pitchfork_blocks_field_groups' );
 add_filter( 'acf/settings/save_json/key=group_6250abdba7d2c', 'pitchfork_blocks_field_groups' );
 add_filter( 'acf/settings/save_json/key=group_601db250e6c94', 'pitchfork_blocks_field_groups' );
-
+add_filter( 'acf/settings/save_json/key=group_6605e470e665f', 'pitchfork_blocks_field_groups' );
+add_filter( 'acf/settings/save_json/key=group_661d8dcf74644', 'pitchfork_blocks_field_groups' );
+add_filter( 'acf/settings/save_json/key=group_6610755895003', 'pitchfork_blocks_field_groups' );
+add_filter( 'acf/settings/save_json/key=group_66196d432370c', 'pitchfork_blocks_field_groups' );
+add_filter( 'acf/settings/save_json/key=group_6619bf2ed4349', 'pitchfork_blocks_field_groups' );
