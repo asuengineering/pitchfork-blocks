c11c39e1095c4766655ba974511d2e7ef83a069b
Steve Ryan <steve.ryan@asu.edu>
2024-04-22 16:59:51 -0700

    Temp uptick version to 2.0.3.
    Release only available on this branch.

--- pitchfork-blocks.php
+++ pitchfork-blocks.php
@@ -6,7 +6,7 @@
  * Author:          Steve Ryan (ASU Engineering)
  * Author URI:      https://engineering.asu.edu
  * Text Domain:     pitchfork-blocks
- * Version:         2.0.2
+ * Version:         2.0.3
  *
  * @package         Pitchfork_Blocks
  *
