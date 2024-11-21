4f40276b576132283613de0637490b153d9d0af5
Steve Ryan <steve.ryan@asu.edu>
2024-11-20 17:06:03 -0700

    Adjust acf/breadcrumb to include wrapper css class

--- acf-block-templates/breadcrumb/breadcrumb.php
+++ acf-block-templates/breadcrumb/breadcrumb.php
@@ -6,6 +6,11 @@
  * @package Pitchfork_Blocks
  */
 
+$block_classes = '';
+// Add additional block styles to invoked class or default crumb.
+if ( isset( $block['className'] ) && ! empty( $block['className'] ) ) {
+	$block_classes = trim( sanitize_text_field( $block['className'] ) );
+}
 
 $crumbs = array(
 	'list_tag'    => 'ul',
@@ -13,13 +18,17 @@ $crumbs = array(
 	'list_class'  => 'breadcrumb',
 	'item_class'  => 'breadcrumb-item',
 	'title_class' => 'd-none',
+	'container_class' => 'breadcrumbs ' . $block_classes
 );
+
+
+
 
 // Display a fake breadcrumb on preview since class is not loaded in admin.
 // Display the real crumb when not in preview mode.
 // See: https://support.advancedcustomfields.com/forums/topic/register-block-preview-image-with-acf_register_block_type/
 
-if ( ! empty( $_POST['query']['preview'] ) ) {
+if ( $is_preview ) {
 
 	?>
 	<ol class="breadcrumb bg-white">
